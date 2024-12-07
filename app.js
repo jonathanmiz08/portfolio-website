915662733c00c008986c25e3cc9ae52f1cde1997
Jonathan Mizhirumbay <107639163+jonathanmiz08@users.noreply.github.com>
2024-12-07 18:47:14 -0500

    ADD EVENT LISTENERS FOR PROJECTS SECTION These event listeners will allow user to toggle a different window within the Projects Section

--- app.js
+++ app.js
@@ -5,3 +5,22 @@ menu.addEventListener('click', function () {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active');
 })
+
+const tab1 = document.querySelector('#fhillc-tab')
+const tab2 = document.querySelector('#portfolio-tab')
+
+
+tab1.addEventListener('click', function () {
+    if (tab1.classList.contains('hidden')) {
+        document.querySelectorAll('.tab, .selected-tab, .content, .nav-url, .description-written')
+            .forEach(tab => {tab.classList.toggle('hidden');})
+    }
+})
+
+tab2.addEventListener('click', function () {
+    if (tab2.classList.contains('hidden')) {
+        document.querySelectorAll('.tab, .selected-tab, .content, .nav-url, .description-written')
+            .forEach(tab => {tab.classList.toggle('hidden');})
+    }
+})
+
